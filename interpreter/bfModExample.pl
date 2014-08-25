c5b93186e5dc1be2847881001f0ce5c3326c9dc2
Zafar Takhirov <z.tahirov@gmail.com>
2014-08-25 11:34:58 -0700

    AUTOCOMMIT: Mon Aug 25 11:34:58 PDT 2014

--- interpreter/bfModExample.pl
+++ interpreter/bfModExample.pl
@@ -7,10 +7,16 @@ BEGIN {push @INC, './'}
 # Using the Brainfuck library:
 use Brainfuck;
 
-
+# Load the BF file:
 my $fh = Brainfuck::bfLoad("zafar.bf");
+
+# Clean up all the comments:
 my @c = Brainfuck::bfClean($fh);
+
+# Convert the code to C:
 my @cc = Brainfuck::bfBody(@c);
+
+# Write the C code (including the header/footer)
 Brainfuck::bfWrite("zafar.c", @cc);
 
 print "\nConversion complete!\n";
