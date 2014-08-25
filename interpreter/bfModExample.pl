de1932bc03462cbdaa7d0d098d299d3d2bb71455
Zafar Takhirov <z.tahirov@gmail.com>
2014-08-25 11:33:59 -0700

    AUTOCOMMIT: Mon Aug 25 11:33:59 PDT 2014

--- interpreter/bfModExample.pl
+++ interpreter/bfModExample.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/env perl
-
 # This script converts zafar.bf to zafar.c
+
+# Add the current directory to the path:
 BEGIN {push @INC, './'}
+
+# Using the Brainfuck library:
 use Brainfuck;
+
+
 my $fh = Brainfuck::bfLoad("zafar.bf");
 my @c = Brainfuck::bfClean($fh);
 my @cc = Brainfuck::bfBody(@c);
